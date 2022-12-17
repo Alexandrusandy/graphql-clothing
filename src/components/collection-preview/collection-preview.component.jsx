18703e870e8aaf2872e7fbf27b8159dc2d504248
Alexandru <alexandrundy@gmail.com>
2022-12-17 14:09:37 +0200

    adding file for graphql

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,26 +1,30 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom';
+import { useNavigate, withRouter } from 'react-router-dom';
 
 import CollectionItem from '../collection-item/collection-item.component';
 
 import './collection-preview.styles.scss';
 
-const CollectionPreview = ({ title, items, history, match }) => (
-  <div className='collection-preview'>
-    <h1
-      className='title'
-      onClick={() => history.push(`${match.path}/${title.toLowerCase()}`)}
-    >
-      {title.toUpperCase()}
-    </h1>
-    <div className='preview'>
-      {items
-        .filter((item, idx) => idx < 4)
-        .map(item => (
-          <CollectionItem key={item.id} item={item} />
-        ))}
-    </div>
-  </div>
-);
+const CollectionPreview = ({ title, items, match }) => {
+  const navigate = useNavigate();
 
-export default withRouter(CollectionPreview);
+  return (
+    <div className="collection-preview">
+      <h1
+        className="title"
+        onClick={() => navigate(`${match.path}/${title.toLowerCase()}`)}
+      >
+        {title.toUpperCase()}
+      </h1>
+      <div className="preview">
+        {items
+          .filter((item, idx) => idx < 4)
+          .map((item) => (
+            <CollectionItem key={item.id} item={item} />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default CollectionPreview;
