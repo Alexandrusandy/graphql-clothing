9f4d2088fbdd3cb5d106ed545bf5f6571e8d6f42
Alexandru <alexandrundy@gmail.com>
2022-12-17 14:35:10 +0200

    configure apollo provider & dependencies

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useNavigate, withRouter } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import CollectionItem from '../collection-item/collection-item.component';
 
--- src/index.js
+++ src/index.js
@@ -8,14 +8,53 @@ import { store, persistor } from './redux/store';
 
 import './index.css';
 import App from './App';
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  gql,
+  InMemoryCache,
+} from '@apollo/client';
+
+const httpLink = createHttpLink({
+  uri: 'https://crwn-clothing.com',
+});
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache,
+});
+
+client
+  .query({
+    query: gql`
+      {
+        getCollectionsByTitle(title: "hats") {
+          id
+          title
+          items {
+            id
+            name
+            price
+            imageUrl
+          }
+        }
+      }
+    `,
+  })
+  .then((res) => console.log('res', res));
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
-    </BrowserRouter>
-  </Provider>,
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
+      </BrowserRouter>
+    </Provider>
+  </ApolloProvider>,
   document.getElementById('root')
 );
